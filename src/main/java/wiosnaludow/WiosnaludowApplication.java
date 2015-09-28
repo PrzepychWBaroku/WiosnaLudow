b2c1ef10407193ed3398b0910f9498d9ffeb2fcd
= <michal.pomaranski@gmail.com>
2015-09-28 22:56:46 +0200

    initial commit

---
+++ src/main/java/wiosnaludow/WiosnaludowApplication.java
@@ -0,0 +1,12 @@
+package wiosnaludow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiosnaludowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiosnaludowApplication.class, args);
+    }
+}
---
+++ src/test/java/wiosnaludow/WiosnaludowApplicationTests.java
@@ -0,0 +1,18 @@
+package wiosnaludow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WiosnaludowApplication.class)
+@WebAppConfiguration
+public class WiosnaludowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
